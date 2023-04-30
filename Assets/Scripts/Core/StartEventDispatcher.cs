ff37ec72bf7e20f3199f387afb68c6be9743c580
Sardorbek <esemxauz@gmail.com>
2023-04-30 16:14:51 +0500

    Project uploading

---
+++ Assets/Scripts/Core/StartEventDispatcher.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Core
+{
+    public class StartEventDispatcher : MonoBehaviour
+    {
+        [SerializeField] private UnityEvent _started;
+
+        private void Start()
+        {
+            _started.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/Gameplay/Timer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace Gameplay
+{
+    public class Timer : MonoBehaviour
+    {
+        public void StartTimer()
+        {
+            
+        }
+    }
+}
